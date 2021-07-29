4b8ff9859b3747c3db6a7380f55f79e4af36fbbc
kylediaz <kyle@kylediaz.com>
2021-07-29 14:31:36 -0400

    Added library for gesture sliding between activities

--- app/src/main/java/com/kylediaz/fbu/dipole_news/activities/ArticleListActivity.java
+++ app/src/main/java/com/kylediaz/fbu/dipole_news/activities/ArticleListActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.klinker.android.sliding.SlidingActivity;
 import com.kylediaz.fbu.dipole_news.R;
 import com.kylediaz.fbu.dipole_news.adapters.ArticleAdapter;
 import com.kylediaz.fbu.dipole_news.models.Article;
@@ -17,7 +18,7 @@ import com.kylediaz.fbu.dipole_news.network.DipoleNewsClient;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ArticleListActivity extends AppCompatActivity {
+public class ArticleListActivity extends SlidingActivity {
 
     private final static String TAG = ArticleListActivity.class.toString();
 
@@ -35,6 +36,16 @@ public class ArticleListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_list);
+
+        Intent intent = getIntent();
+        if (intent.getBooleanExtra(SampleActivity.ARG_USE_EXPANSION, false)) {
+            expandFromPoints(
+                    intent.getIntExtra(SampleActivity.ARG_EXPANSION_LEFT_OFFSET, 0),
+                    intent.getIntExtra(SampleActivity.ARG_EXPANSION_TOP_OFFSET, 0),
+                    intent.getIntExtra(SampleActivity.ARG_EXPANSION_VIEW_WIDTH, 0),
+                    intent.getIntExtra(SampleActivity.ARG_EXPANSION_VIEW_HEIGHT, 0)
+            );
+        }
 
         rvArticleList = findViewById(R.id.rvArticleList);
         rvArticleList.setLayoutManager(new LinearLayoutManager(this));
@@ -60,5 +71,11 @@ public class ArticleListActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void configureScroller(MultiShrinkScroller scroller) {
+        super.configureScroller(scroller);
+        scroller.setIntermediateHeaderHeightRatio(1);
     }
 }
