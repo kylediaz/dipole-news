0bc2955c62183a66347dd696189e341323fdc81c
kylediaz <kyle@kylediaz.com>
2021-08-02 18:19:19 -0500

    Sort events with toolbar at top

---
+++ app/src/main/java/com/kylediaz/fbu/dipole_news/activities/PublisherBlacklistActivity.java
@@ -0,0 +1,17 @@
+package com.kylediaz.fbu.dipole_news.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kylediaz.fbu.dipole_news.R;
+
+public class PublisherBlacklistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publisher_blacklist);
+    }
+
+}
